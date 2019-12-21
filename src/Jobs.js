c0d084329e12003c96229fab07425fcc1e194a9d
Deepak Surya <ideepaksuryas@gmail.com>
2019-12-21 12:18:34 +0530

    initial commit

---
+++ src/Jobs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+import Job from './Job';
+
+export default function Jobs() {
+  return (
+    <div className = "jobs">
+      <Typography variant="h1">
+        Entry Level Software Jobs
+      </Typography>
+    </div>
+  )
+}
