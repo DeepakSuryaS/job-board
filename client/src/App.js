748ae1b4929386ee58cfabd1c0021878510d81bc
Deepak Surya <ideepaksuryas@gmail.com>
2019-12-21 20:54:37 +0530

    receive res from api, set state jobs list

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import Jobs from "./Jobs"
+
+const JOB_API_URL = 'http://localhost:3001/jobs';
 
 const mockJobs = [
   {id: 'swe1', title: 'SWE 1', company: 'Google'},
@@ -10,10 +12,23 @@ const mockJobs = [
   {id: 'swe4', title: 'SWE 4', company: 'Tesla'},
 ]
 
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+  updateCb(json); // updateJobs 
+}
+
 function App() {
+
+  const [jobsList, updateJobs] = useState([]);
+
+  useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <div className="App">
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobsList} />
     </div>
   );
 }
